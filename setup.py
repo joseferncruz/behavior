c019216fbf28f4ad479a2542e5d58059d729ff32
JC <29801616+joseferncruz@users.noreply.github.com>
2020-05-28 19:52:50 -0400

    remove __main__

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_namespace_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -14,23 +14,23 @@ requirements = [
     "sklearn",
 ]
 
-if __name__ == "__main__":
-    setup(
-        name='Behavior',
-        version='0.0.1',
-        description='Code to analyse behavior',
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        author='Jose Cruz, PhD',
-        author_email='jose.cruz@nyu.edu',
-        url='https://github.com/joseferncruz/behavior',
-        packages=find_namespace_packages(include=["behavior.*"],
-        include_package_data=True,
-        install_requires=requirements,
-        license='MIT',
-        python_requires='>=3.6',
-        zip_safe=False,
-    )
+#if __name__ == "__main__":
+setup(
+    name='Behavior',
+    version='0.0.1',
+    description='Code to analyse behavior',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jose Cruz, PhD',
+    author_email='jose.cruz@nyu.edu',
+    url='https://github.com/joseferncruz/behavior',
+    packages=find_namespace_packages(include=["behavior.*"],
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    python_requires='>=3.6',
+    zip_safe=False,
+)
 
 ########## More information
 # https://python-packaging-user-guide.readthedocs.io/tutorials/packaging-projects/
