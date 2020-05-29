fe749f95e2eeb9704dde72adbbd8ed212285b14a
JC <29801616+joseferncruz@users.noreply.github.com>
2020-05-29 09:56:04 -0400

    minor addition to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -14,20 +14,19 @@ requirements = [
     "sklearn",
 ]
 
-#if __name__ == "__main__":
-setup(
-    name='Behavior',
-    version='0.0.1',
-    description='Code to analyse behavior',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Jose Cruz, PhD',
-    author_email='jose.cruz@nyu.edu',
-    url='https://github.com/joseferncruz/behavior',
-    packages=find_namespace_packages(include=["behavior.*"],
-    include_package_data=True,
-    install_requires=requirements,
-    license='MIT',
-    python_requires='>=3.6',
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup(
+        name='Behavior',
+        version='0.0.1',
+        description='Code to analyse behavior',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Jose Cruz, PhD',
+        author_email='jose.cruz@nyu.edu',
+        url='https://github.com/joseferncruz/behavior',
+        packages=find_packages(), #find_namespace_packages(include=["behavior.*"],
+        include_package_data=True,
+        install_requires=requirements,
+        license='MIT',
+        python_requires='>=3.6',
+        zip_safe=False)
